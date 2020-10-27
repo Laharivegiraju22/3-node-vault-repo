414c70aee287e00176164742465711bd4607e568
natam369 <58681656+natam369@users.noreply.github.com>
2020-10-27 17:24:01 +0530

    Update main.tf

--- aws-withvault/vault-adminspace/main.tf
+++ aws-withvault/vault-adminspace/main.tf
@@ -44,10 +44,55 @@ resource "vault_aws_secret_backend_role" "admin" {
 }
 EOF
 }
+resource "vault_aws_secret_backend_role" "s3user" {
+  backend = vault_aws_secret_backend.aws.path
+  name    = "s3-role"
+  credential_type = "iam_user"
+  policy_document = <<EOF
+{
+     "Version": "2012-10-17",
+     "Statement": [
+        {
+          "Effect": "Allow",
+          "Action": [
+          "iam:*", "s3:*"
+         ],
+          "Resource": "*"
+        }
+      ]
+}
+EOF
+}
+resource "vault_aws_secret_backend_role" "eksuser" {
+  backend = vault_aws_secret_backend.aws.path
+  name    = "eks-role"
+  credential_type = "iam_user"
+  policy_document = <<EOF
+{
+     "Version": "2012-10-17",
+     "Statement": [
+        {
+          "Effect": "Allow",
+          "Action": [
+          "iam:*", "eks:*"
+        ],
+         "Resource": "*"
+       }
+     ]
+}
+EOF
+}
+
 output "backend" {
   value = vault_aws_secret_backend.aws.path
 }
 
 output "role" {
   value = vault_aws_secret_backend_role.admin.name
+}
+output "s3-role" {
+  value = vault_aws_secret_backend_role.s3user.name
+}
+output "EKS-role" {
+  value = vault_aws_secret_backend_role.eksuser.name
 }
